03a1abda9dc7f10ede3e4f48aa4bd184e00c8eff
ChaXxl <x734461204@gmail.com>
2024-08-08 22:31:37 +0800

    docs: 更新翻译

--- src/AlwaysEnglish_en_US.ts
+++ src/AlwaysEnglish_en_US.ts
@@ -28,71 +28,71 @@
 <context>
     <name>home</name>
     <message>
-        <location filename="qml/page/home.qml" line="23"/>
+        <location filename="qml/page/home.qml" line="22"/>
         <source>App-specific settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="91"/>
+        <location filename="qml/page/home.qml" line="90"/>
         <source>Remove</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="115"/>
-        <location filename="qml/page/home.qml" line="128"/>
+        <location filename="qml/page/home.qml" line="114"/>
+        <location filename="qml/page/home.qml" line="127"/>
         <source>Start</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="115"/>
-        <location filename="qml/page/home.qml" line="122"/>
-        <location filename="qml/page/home.qml" line="161"/>
+        <location filename="qml/page/home.qml" line="114"/>
+        <location filename="qml/page/home.qml" line="121"/>
+        <location filename="qml/page/home.qml" line="160"/>
         <source>Stop</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="126"/>
+        <location filename="qml/page/home.qml" line="125"/>
         <source>Start Successfully</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="157"/>
-        <location filename="qml/page/home.qml" line="164"/>
+        <location filename="qml/page/home.qml" line="156"/>
+        <location filename="qml/page/home.qml" line="163"/>
         <source>AlwaysEnglish</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="171"/>
+        <location filename="qml/page/home.qml" line="170"/>
         <source>Keep English All The Time</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="191"/>
+        <location filename="qml/page/home.qml" line="190"/>
         <source>Add an APP</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="200"/>
+        <location filename="qml/page/home.qml" line="199"/>
         <source>select an APP</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="222"/>
+        <location filename="qml/page/home.qml" line="221"/>
         <source>icon</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="233"/>
+        <location filename="qml/page/home.qml" line="232"/>
         <source>Turn on</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="237"/>
+        <location filename="qml/page/home.qml" line="236"/>
         <source>Cap Lock</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="241"/>
+        <location filename="qml/page/home.qml" line="240"/>
         <source>Options</source>
         <translation type="unfinished"></translation>
     </message>
@@ -100,37 +100,37 @@
 <context>
     <name>settings</name>
     <message>
-        <location filename="qml/page/settings.qml" line="8"/>
+        <location filename="qml/page/settings.qml" line="9"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="23"/>
+        <location filename="qml/page/settings.qml" line="24"/>
         <source>Dark Mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="28"/>
+        <location filename="qml/page/settings.qml" line="29"/>
         <source>System</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="28"/>
+        <location filename="qml/page/settings.qml" line="29"/>
         <source>Light</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="28"/>
+        <location filename="qml/page/settings.qml" line="29"/>
         <source>Dark</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="46"/>
+        <location filename="qml/page/settings.qml" line="47"/>
         <source>Sticky on Top</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="58"/>
+        <location filename="qml/page/settings.qml" line="59"/>
         <source>Whether turn on Cap Lock when AlwaysEnglish is on</source>
         <translation type="unfinished"></translation>
     </message>
--- src/AlwaysEnglish_zh_CN.ts
+++ src/AlwaysEnglish_zh_CN.ts
@@ -28,72 +28,72 @@
 <context>
     <name>home</name>
     <message>
-        <location filename="qml/page/home.qml" line="23"/>
+        <location filename="qml/page/home.qml" line="22"/>
         <source>App-specific settings</source>
         <translation type="unfinished">APP 设置</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="91"/>
+        <location filename="qml/page/home.qml" line="90"/>
         <source>Remove</source>
         <translation type="unfinished">移除</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="115"/>
-        <location filename="qml/page/home.qml" line="128"/>
+        <location filename="qml/page/home.qml" line="114"/>
+        <location filename="qml/page/home.qml" line="127"/>
         <source>Start</source>
         <translation type="unfinished">启动</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="157"/>
-        <location filename="qml/page/home.qml" line="164"/>
+        <location filename="qml/page/home.qml" line="156"/>
+        <location filename="qml/page/home.qml" line="163"/>
         <source>AlwaysEnglish</source>
         <translation type="unfinished">始终为英文</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="115"/>
-        <location filename="qml/page/home.qml" line="122"/>
-        <location filename="qml/page/home.qml" line="161"/>
+        <location filename="qml/page/home.qml" line="114"/>
+        <location filename="qml/page/home.qml" line="121"/>
+        <location filename="qml/page/home.qml" line="160"/>
         <source>Stop</source>
         <translation type="unfinished">停    止</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="126"/>
+        <location filename="qml/page/home.qml" line="125"/>
         <source>Start Successfully</source>
         <translation type="unfinished">启动成功</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="171"/>
+        <location filename="qml/page/home.qml" line="170"/>
         <source>Keep English All The Time</source>
         <translation type="unfinished">无论什么软件, 一直保持英文输入法（在设置里决定是否打开大小写键）</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="191"/>
+        <location filename="qml/page/home.qml" line="190"/>
         <source>Add an APP</source>
         <translation type="unfinished">添加应用</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="200"/>
+        <location filename="qml/page/home.qml" line="199"/>
         <source>select an APP</source>
         <translation type="unfinished">选择一个 APP</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="222"/>
+        <location filename="qml/page/home.qml" line="221"/>
         <source>icon</source>
         <translation type="unfinished">应用图标</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="233"/>
+        <location filename="qml/page/home.qml" line="232"/>
         <source>Turn on</source>
         <translation type="unfinished">启用</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="237"/>
+        <location filename="qml/page/home.qml" line="236"/>
         <source>Cap Lock</source>
         <oldsource>Caps</oldsource>
         <translation type="unfinished">大小写键</translation>
     </message>
     <message>
-        <location filename="qml/page/home.qml" line="241"/>
+        <location filename="qml/page/home.qml" line="240"/>
         <source>Options</source>
         <translation type="unfinished">操作</translation>
     </message>
@@ -101,37 +101,37 @@
 <context>
     <name>settings</name>
     <message>
-        <location filename="qml/page/settings.qml" line="8"/>
+        <location filename="qml/page/settings.qml" line="9"/>
         <source>Settings</source>
         <translation type="unfinished">设置</translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="23"/>
+        <location filename="qml/page/settings.qml" line="24"/>
         <source>Dark Mode</source>
         <translation type="unfinished">夜间模式</translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="28"/>
+        <location filename="qml/page/settings.qml" line="29"/>
         <source>System</source>
         <translation type="unfinished">跟随系统</translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="28"/>
+        <location filename="qml/page/settings.qml" line="29"/>
         <source>Light</source>
         <translation type="unfinished">浅色</translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="28"/>
+        <location filename="qml/page/settings.qml" line="29"/>
         <source>Dark</source>
         <translation type="unfinished">深色</translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="46"/>
+        <location filename="qml/page/settings.qml" line="47"/>
         <source>Sticky on Top</source>
         <translation type="unfinished">窗口置顶</translation>
     </message>
     <message>
-        <location filename="qml/page/settings.qml" line="58"/>
+        <location filename="qml/page/settings.qml" line="59"/>
         <source>Whether turn on Cap Lock when AlwaysEnglish is on</source>
         <translation type="unfinished">当一直开启英文输入法时，是否一直打开大小写键</translation>
     </message>
